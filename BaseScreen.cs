22ec5bf6df4590bcc95cd7d9196ef62b2d3fec48
Anton Samartsev <coderubrika@gmail.com>
2023-06-24 13:32:54 +0300

    improved

--- BaseScreen.cs
+++ BaseScreen.cs
@@ -7,16 +7,12 @@ namespace Suburb.Screens
     {
         protected bool isShow;
 
-        public readonly ReactiveCommand OnShowEnd = new();
-        public readonly ReactiveCommand OnHideEnd = new();
-
         public void InitShow()
         {
             if (isShow)
                 return;
 
             isShow = true;
-
             Show();
         }
 
@@ -26,20 +22,17 @@ namespace Suburb.Screens
                 return;
 
             isShow = false;
-
             Hide();
         }
 
         protected virtual void Show()
         {
             gameObject.SetActive(true);
-            OnShowEnd.Execute();
         }
 
         protected virtual void Hide()
         {
             gameObject.SetActive(false);
-            OnHideEnd.Execute();
         }
     }
 }
