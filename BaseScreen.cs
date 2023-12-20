569da95a5a2e7bde4cd19c4106952153daee8da9
Anton Samartsev <coderubrika@gmail.com>
2023-12-20 19:12:26 +0300

    in process maybe not working

--- BaseScreen.cs
+++ BaseScreen.cs
@@ -1,23 +1,11 @@
 using Suburb.ExpressRouter;
 using Suburb.Cameras;
 using UnityEngine;
-using Zenject;
 
 namespace Suburb.Screens
 {
     public abstract class BaseScreen : MonoBehaviour, IEndpoint
     {
-        protected CameraService cameraService;
-        
-        [SerializeField] protected Canvas canvas;
-        
-        [Inject]
-        public void Construct(CameraService cameraService)
-        {
-            this.cameraService = cameraService;
-            canvas.worldCamera = cameraService.GetCamera("UICamera");
-        }
-        
         public bool IsShow { get; private set; }
 
         public string Name => GetType().Name;
